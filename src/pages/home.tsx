ec1d2c02a4df08c9fd86868392b0a312188650fd
AliSiddique <alisiddique10@hotmail.com>
2023-05-04 17:21:51 +0100

    updated

---
+++ src/pages/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import Filter from '../../components/Filter'
+
+import { FunctionComponent } from 'react';
+
+const Nice: FunctionComponent = () => {
+  return (
+    <div>
+        <Navbar />
+        <Filter/>
+    </div>
+  );
+};
+
+export default Nice;
